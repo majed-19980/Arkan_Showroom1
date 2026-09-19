ece5c73e91c8fbf9f2bfbbbcc8c04f901efcc401
majed-19980 <majeed1998m@gmail.com>
2026-09-19 20:01:13 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='arkan-showroom-v5';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./index.html','./manifest.json']))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
